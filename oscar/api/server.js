2362ed2c4da144c7e3aaebe7bae4a41a30de4055
quantranld <qmenq07@gmail.com>
2020-03-27 10:26:05 +0800

    + m1 -> m3

---
+++ oscar/api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const auth = require('./routes/api/auth');
+
+const app = express();
+
+// connect DB
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+app.use('/api/auth', auth);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
